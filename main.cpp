48f3280ec5e5e4b3ea0f5c324345284362f75a6c
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2024-12-17 00:58:51 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <QApplication>
-#include "overviewWidget.hpp"
+#include "PCBMonitor.hpp"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     
-    OverviewWidget widget;
-    widget.setWindowTitle("Pollutant Overview"); // Set the window title
-    widget.setMinimumSize(800, 600);  // Set minimum size
-    widget.resize(1024, 768);         // Set default size
-    widget.show();
+    PCBMonitor monitor;
+    monitor.setWindowTitle("PCB Monitoring");
+    monitor.resize(1024, 768);
+    monitor.show();
     
     return app.exec();
 }
