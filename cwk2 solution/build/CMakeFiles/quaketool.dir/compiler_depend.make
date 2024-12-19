f5f14534ce8be50ef4f2a0d9ecb49cbd97c43c08
finlay.ab <finlay.brydges@live.co.uk>
2024-12-19 13:01:16 +0000

    removed build

--- cwk2 solution/build/CMakeFiles/quaketool.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for quaketool.
-# This may be replaced when dependencies are built.
--- cwk2 solution/build/CMakeFiles/quaketool_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/quaketool_autogen"
-  "quaketool_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/quaketool_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
